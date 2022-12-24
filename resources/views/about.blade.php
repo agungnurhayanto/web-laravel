a62e7c830552c7ae711671674b4b50578e726bba
alinvanhouten <agung.nex.edp@gmail.com>
2022-12-24 19:31:50 +0700

    bikin view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, in
+    itial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Agung Blog : About</title>
+</head>
+
+<body>
+    <h1>Halaman About</h1>
+    <h3>Agung Nurhayanto</h3>
+    <p>agung.nex.edp@gmail.com</p>
+    <img src="img/yuda.jpg" alt="Yuda Hari Subekti" width="200">
+    <script src="js/script.js"></script>
+</body>
+
+</html>
